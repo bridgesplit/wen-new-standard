549382911040af696c99292cfe4cc6e8537ae537
Bhargava Sai Macha <mbsai29@gmail.com>
2024-03-04 22:08:34 -0500

    improvements

--- programs/wen-royalty-distribution/src/errors.rs
+++
@@ -1,7 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[error_code]
-pub enum DistributionErrors {
-    #[msg("Invalid creator pct amount. Must add up to 100")]
-    InvalidCreatorPctAmount,
-}
---
+++ programs/wen_royalty_distribution/src/errors.rs
@@ -0,0 +1,9 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum DistributionErrors {
+    #[msg("Invalid creator pct amount. Must add up to 100")]
+    InvalidCreatorPctAmount,
+    #[msg("Arithmetic overflow")]
+    ArithmeticOverflow,
+}
